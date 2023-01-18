110da1b90ea2d10a194cb09754489f9ff04bcba2
Sarr <contact@amadousarr.com>
2023-01-18 14:18:12 +0100

    Using Form Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,15 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  submitted = false;
 
+  user= {
+    username: '',
+    email: '',
+    secretQuestion:'',
+    answer: '',
+    gender: ''
+  }
   suggestUserName() {
     const suggestedName = 'Superuser';
     /* this.signupForm.setValue({
@@ -35,6 +43,12 @@ export class AppComponent {
   }
 
   onSubmit() {
+    this.submitted = true;
     console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
